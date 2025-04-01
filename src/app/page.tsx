a6f466f48b637dc919f8fdae58e3084993b709c3
Korvo2irow <korvo2irow@live.com>
2025-04-01 14:18:38 -0300

    css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,19 +23,21 @@ export default function Home() {
 
         <Banner />
 
-     
 
-        <div className="bg-white/10 backdrop-blur-md border text-lg border-yellow-300 text-white p-5 rounded-lg w-full max-w-[1000px]">
 
-        <h1 className=" text-4xl text-white flex justify-center items-center mb-5">🍔🔥 Bem-vindo ao Point do Canal – O Sabor que Conquista!</h1>
-         
-        <p>
-          Se você busca hambúrgueres suculentos com pão de fabricação própria, pastéis crocantes, caldos irresistíveis e aquele açaí cremoso para refrescar o dia, você está no lugar certo! 😍
+        <div className="bg-white/30 backdrop-blur-md border-2 text-lg border-yellow-400 text-white p-5 rounded-lg w-full max-w-[1000px] shadow-2xl shadow-yellow-400">
+
+          <h1 className=" text-4xl text-white flex justify-center items-center mb-5 text-center">🍔🔥 Bem-vindo ao Point do Canal – O Sabor que Conquista!</h1>
+
+          <p>
+            Se você busca hambúrgueres suculentos com pão de fabricação própria, pastéis crocantes, caldos irresistíveis e aquele açaí cremoso para refrescar o dia, você está no lugar certo! 😍
             Aqui, cada detalhe faz a diferença. Nosso pão é artesanal, feito diariamente para garantir frescor e sabor incomparáveis. Cada ingrediente é escolhido com carinho, resultando em uma experiência gastronômica única e inesquecível.
             📍 Peça agora e descubra o verdadeiro sabor!
             🚀 Delivery rápido | 🏠 Ambiente aconchegante | ❤️ Feito com paixão
             🔽 Faça seu pedido agora mesmo!
-        </p></div>
+          </p>
+
+        </div>
 
       </div>
     </div>
