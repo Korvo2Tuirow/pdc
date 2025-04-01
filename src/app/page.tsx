a629151ec9cc5843be698c031a4301fa0bcf736f
Korvo2irow <korvo2irow@live.com>
2025-04-01 17:32:18 -0300

    teste links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 
+import Image from "next/image";
 import { Banner } from "./components/banner";
 import { Container } from "./components/containers";
 
@@ -19,15 +20,45 @@ export default function Home() {
         ))}
       </section>
 
-      <div className="z-10 flex p-10 gap-24 flex-col absolute w-full justify-center items-center pointer-events-none ">
+      <div className="z-10 flex flex-col absolute w-full justify-center items-center pointer-events-none">
+
+        <nav className="bg-black/30 backdrop-blur-md flex justify-center items-center text-lg text-white p-5  rounded-lg w-full max-w-[1000px]">
+
+          <div className="flex-1 flex">
+            
+            <a href="#qsomo" className="cursor-pointer pointer-events-auto bg-yellow-300 text-black backdrop-blur-md border-2 text-lg border-yellow-400 p-1 rounded-lg ">Quem somos !</a>
+          
+          <a href="#qsomo" className="cursor-pointer pointer-events-auto bg-yellow-300 text-black backdrop-blur-md border-2 text-lg border-yellow-400 p-1 rounded-lg">Cardapio</a>
+          
+          </div>
+
+       <div className="bg-red-500 p-4 rounded-full ">
+         
+              <Image
+                src={"/ifood-logo-7.png"}
+                width={80}
+                height={80}
+                alt="ifood"
+                className="cursor-pointer pointer-events-auto"
+         
+              />
+       </div>
+  
+
+          <Image
+            src={"/whatsapp1.png"}
+            width={120}
+            height={120}
+            alt="whtassapp"
+            className="p-5 rounded-lg cursor-pointer pointer-events-auto"
+          />
+        </nav>
 
         <Banner />
 
+        <div className="bg-white/30 backdrop-blur-md border-2 text-lg border-yellow-400 text-white p-5 rounded-lg w-full max-w-[1000px] shadow-2xl shadow-yellow-400" id="qsomo">
 
-
-        <div className="bg-white/30 backdrop-blur-md border-2 text-lg border-yellow-400 text-white p-5 rounded-lg w-full max-w-[1000px] shadow-2xl shadow-yellow-400">
-
-          <h1 className=" text-4xl text-white flex justify-center items-center mb-5 text-center">🍔🔥 Bem-vindo ao Point do Canal – O Sabor que Conquista!</h1>
+          <h1 className=" text-4xl text-white flex justify-center items-center mb-5 text-center qsomo">🍔🔥 Bem-vindo ao Point do Canal – O Sabor que Conquista!</h1>
 
           <p>
             Se você busca hambúrgueres suculentos com pão de fabricação própria, pastéis crocantes, caldos irresistíveis e aquele açaí cremoso para refrescar o dia, você está no lugar certo! 😍
