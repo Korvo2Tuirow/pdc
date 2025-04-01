e9ad3bd9de9e9721ef8776ef6ed3bda3c9837580
Korvo2irow <korvo2irow@live.com>
2025-04-01 18:09:33 -0300

    menu css bg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
       <div className="z-10  flex flex-col absolute w-full justify-center items-center pointer-events-none">
 
-        <nav className="bg-black/30 backdrop-blur-md flex justify-center items-center text-lg text-white p-5  rounded-lg w-full max-w-[1000px]">
+        <nav className="bg-black/10 flex justify-center items-center text-lg text-white p-5  rounded-lg w-full max-w-[1000px]">
 
           <div className="flex-1 flex">
             {/*
