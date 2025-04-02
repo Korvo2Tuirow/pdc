f31b65d54b8010850949411f117209b4c9dc9280
Korvo2irow <korvo2irow@live.com>
2025-04-02 01:23:02 -0300

    css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,13 +84,13 @@ export default function Home() {
         <section className="bg-black/30 gap-5 flex flex-col sticky my-24 justify-center items-center text-white p-5  w-auto max-w-[1000px]" >
 
           <div className="flex gap-3 w-full">
-            <Image src={"/h01.jpg"} width={150} height={1} alt="h1" className="shadow-2xl shadow-yellow-400 rounded-2xl border-2 border-yellow-300" />
-            <Image src={"/h04.jpg"} width={150} height={1} alt="h4" className="shadow-2xl shadow-yellow-400 rounded-2xl border-2 border-yellow-300" />
+            <Image src={"/h01.jpg"} width={150} height={1} alt="h1" className="shadow-2xl shadow-yellow-400 border border-yellow-300" />
+            <Image src={"/h04.jpg"} width={150} height={1} alt="h4" className="shadow-2xl shadow-yellow-400 border border-yellow-300" />
           </div>
-          <Image src={"/h03.jpg"} width={300} height={1} alt="h3" className="shadow-2xl shadow-yellow-400 rounded-2xl border-2 border-yellow-300" />
+          <Image src={"/h03.jpg"} width={300} height={1} alt="h3" className="shadow-2xl shadow-yellow-400 border border-yellow-300" />
           <div className="flex gap-3">
-            <Image src={"/pr01.jpg"} width={150} height={1} alt="h5" className="shadow-2xl shadow-yellow-400 rounded-2xl border-2 border-yellow-300" />
-            <Image src={"/h05.jpg"} width={150} height={1} alt="h5" className="shadow-2xl shadow-yellow-400 rounded-2xl border-2 border-yellow-300" />
+            <Image src={"/pr01.jpg"} width={150} height={1} alt="h5" className="shadow-2xl shadow-yellow-400 border border-yellow-300" />
+            <Image src={"/h05.jpg"} width={150} height={1} alt="h5" className="shadow-2xl shadow-yellow-400 border border-yellow-300" />
           </div>
 
         </section>
