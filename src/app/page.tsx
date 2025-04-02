75d4fc9da69689fc9ea47b6744d2ab4743ac8fb6
Korvo2irow <korvo2irow@live.com>
2025-04-02 00:10:32 -0300

    foto resp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,16 +80,16 @@ export default function Home() {
 
         </div>
             
-            <div className="bg-black/30 gap-5 flex flex-col  my-24 justify-center items-center text-white p-5  w-auto max-w-[1000px]" >
+            <div className="bg-black/30 gap-5 flex flex-col sticky my-24 justify-center items-center text-white p-5  w-auto max-w-[1000px]" >
             
                 <div className="flex gap-3 w-full">
-                  <Image src={"/h01.jpg"} width={200} height={1} alt="h1" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
-                  <Image src={"/h04.jpg"} width={200} height={1} alt="h4" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
+                  <Image src={"/h01.jpg"} width={150} height={1} alt="h1" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
+                  <Image src={"/h04.jpg"} width={150} height={1} alt="h4" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
                 </div>         
-                <Image src={"/h03.jpg"} width={500} height={1} alt="h3" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
+                <Image src={"/h03.jpg"} width={300} height={1} alt="h3" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
                 <div className="flex gap-3">
-                  <Image src={"/pr01.jpg"} width={200} height={1} alt="h5" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
-                  <Image src={"/h05.jpg"} width={200} height={1} alt="h5" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
+                  <Image src={"/pr01.jpg"} width={150} height={1} alt="h5" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
+                  <Image src={"/h05.jpg"} width={150} height={1} alt="h5" className="shadow-2xl shadow-yellow-400 rounded-2xl"/>
                 </div>
 
             </div>
