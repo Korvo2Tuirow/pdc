5e8a994c5b9979fad8a854b499bf3434bcb0cce2
Korvo2irow <korvo2irow@live.com>
2025-04-09 14:25:48 -0300

    video loop

--- src/app/components/videoMedia.tsx
+++ src/app/components/videoMedia.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 export const VideoPDC = () => {
     return (
-        <video width="1000" height="240" controls autoPlay muted className='pointer-events-auto flex justify-center items-center mt-16 shadow-2xl shadow-blue-400'>
+        <video width="1000" height="240" controls autoPlay muted loop className='pointer-events-auto flex justify-center items-center mt-16 shadow-2xl shadow-blue-400'>
             <source src="/PDCvideo.mp4" type="video/mp4" />
         </video>
     );
