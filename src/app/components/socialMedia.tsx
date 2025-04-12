28660aabf952451624b60932d666b30ceb1e835d
Korvo2irow <korvo2irow@live.com>
2025-04-12 13:05:54 -0300

    social media css

--- src/app/components/socialMedia.tsx
+++ src/app/components/socialMedia.tsx
@@ -3,18 +3,24 @@ import Link from 'next/link';
 import React from 'react';
 export const SocialMedia = () => {
     return (
-        <div className='flex flex-col justify-center items-center w-full max-w-[1000px] mt-16'>
-            <div className='flex flex-wrap justify-center items-center gap-10'>
-            <Link href={"https://www.instagram.com/hamburgueria_point_docanal"} target='_blank'>
-                <Image src={"/instalogo.png"} alt='instagram' width={400} height={100} className='rounded-lg pointer-events-auto'/>
-            </Link>
+        <div className='flex flex-col justify-center items-center w-full max-w-[1000px] mt-16  bg-white rounded-t-lg '>
 
-            <Link href={"https://www.facebook.com/pointdocanal"} target='_blank'>
-                <Image src={"/fblogo.png"} alt='instagram' width={400} height={100} className='rounded-lg pointer-events-auto' />
-            </Link>
+            <h1 className="  text-gray-500 font-bold   pt-8  rounded-lg flex justify-center  w-full max-w-[1000px]  text-2xl">Sigam em nossas redes sociais</h1>
+
+            <hr />
+
+            <div className='flex flex-wrap justify-center py-5 mb- items-center gap-10  h-64'>
+
+                <Link href={"https://www.instagram.com/hamburgueria_point_docanal"} target='_blank'>
+                    <Image src={"/instalogo.png"} alt='instagram' width={400} height={100} className='rounded-lg pointer-events-auto' />
+                </Link>
+
+                <Link href={"https://www.facebook.com/pointdocanal"} target='_blank'>
+                    <Image src={"/fblogo.png"} alt='instagram' width={400} height={100} className='rounded-lg pointer-events-auto ' />
+                </Link>
 
             </div>
-            <video width="1000" height="240" controls autoPlay muted loop className='pointer-events-auto flex justify-center items-center mt-16 shadow-2xl'>
+            <video width="1000" height="240" controls autoPlay muted loop className='pointer-events-auto flex justify-center items-center lg:mt-16 mt-28 shadow-2xl'>
                 <source src="/PDCvideo.mp4" type="video/mp4" />
             </video>
         </div>
