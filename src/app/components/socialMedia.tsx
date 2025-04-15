d544f042baad84993ffe792da03cf7b52c6aef3b
Korvo2irow <korvo2irow@live.com>
2025-04-15 01:23:31 -0300

    social media

--- src/app/components/socialMedia.tsx
+++ src/app/components/socialMedia.tsx
@@ -3,13 +3,19 @@ import Link from 'next/link';
 import React from 'react';
 export const SocialMedia = () => {
     return (
-        <div className='flex flex-col justify-center items-center w-full max-w-[1000px]   bg-white rounded-t-lg '>
 
-            <h1 className="  text-gray-500 font-bold   pt-8  rounded-lg flex justify-center  w-full max-w-[1000px]  text-2xl">Sigam em nossas redes sociais</h1>
+        <>
+            <div className='flex flex-col justify-center p-8 items-center w-full max-w-[1000px] gap-6 bg-white rounded-lg '>
 
-            <hr />
+                <h1 className="  text-gray-500 font-bold    rounded-lg flex justify-center  w-full max-w-[1000px]  text-2xl">Sigam em nossas redes sociais</h1>
 
-            <div className='flex flex-wrap justify-center py-5 mb- items-center gap-10  h-64'>
+          
+
+            <video width="1000" height="240" controls autoPlay muted loop className='pointer-events-auto flex justify-center items-center lg:mt-6  '>
+                <source src="/PDCvideo.mp4" type="video/mp4" />
+            </video>
+
+            <div className='flex flex-wrap justify-center py-5 mb- items-center gap-10  h-auto'>
 
                 <Link href={"https://www.instagram.com/hamburgueria_point_docanal"} target='_blank'>
                     <Image src={"/instalogo.png"} alt='instagram' width={400} height={100} className='rounded-lg pointer-events-auto' />
@@ -20,10 +26,10 @@ export const SocialMedia = () => {
                 </Link>
 
             </div>
-            <video width="1000" height="240" controls autoPlay muted loop className='pointer-events-auto flex justify-center items-center lg:mt-16 mt-28 shadow-2xl'>
-                <source src="/PDCvideo.mp4" type="video/mp4" />
-            </video>
-        </div>
+
+            </div>
+
+        </>
     );
 }
 
